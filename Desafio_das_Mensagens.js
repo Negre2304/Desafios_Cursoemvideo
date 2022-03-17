e637cf89f9350316857daa8555f14d7d9ba46c43
Negre2304 <negre.victor@hotmail.com>
2022-03-17 10:39:04 -0300

    Desafio concluido

---
+++ Desafio_das_Mensagens.js
@@ -0,0 +1,14 @@
+function botao1() {
+
+    alert('Você clicou no 1º Botão!')
+}
+
+function botao2() {
+
+    alert('Você clicou no 2º Botão!')
+}
+
+function botao3() {
+
+    alert('Você clicou no 3º Botão!')
+}
